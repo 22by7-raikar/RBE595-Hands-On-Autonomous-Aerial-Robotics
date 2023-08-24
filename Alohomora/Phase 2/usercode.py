f36dc24dc8aa68693a1061d654253504f0d335f6
Anuj Pai Raikar <117297932+22by7-raikar@users.noreply.github.com>
2023-08-24 18:35:39 -0400

    Update usercode.py

--- Alohomora/Phase 2/usercode.py
+++ Alohomora/Phase 2/usercode.py
@@ -234,6 +234,91 @@ class state_machine:
 
 
 
+# import numpy as np
+# import cv2
+# import bpy
+# import os
+# import copy
+# import math
+# import apriltag
+
+# class state_machine:
+#     def __init__(self):
+#         # User code executes every dt time in seconds
+#         self.dt = 0.050
+#         self.i = 0
+#         # location of each tag
+#         self.waypoints = [
+#             [-1.7,      0.8,    1.5],
+#             [-1.2,     -1.6,      1.5],
+#             [0.9,     0.7,    1.5]
+#         ]
+
+#         # Current waypoint
+#         self.currentWP = [0,    -0.2,    1.5]
+#         self.landed = False
+
+#     def step(self, time, currpos):
+#         """
+#         Input: time, current position in blender frame (x, y, z)
+#         Output: desired position
+#             Si unit unless specified otherwise
+
+#             FILL YOUR CODE HERE!
+#         """
+
+#         # Write your logic here. Do not edit anything else apart from paths
+#         if time < 1:
+#             xyz_desired = self.currentWP
+#             return xyz_desired
+        
+#         if self.landed and (time - curr_time) < 5:
+#             return xyz_desired
+        
+#         num_waypts = len(self.waypoints)
+#         # atwWP = 0
+#         curr_waypt = self.waypoints[self.i]
+#         xyz_desired = curr_waypt
+
+#         cwp_x, cwp_y, cwp_z, = curr_waypt[0], curr_waypt[1], curr_waypt[2]
+#         cp_x, cp_y, cp_z, = currpos[0], currpos[1], currpos[2]
+
+#         euc_dis = math.sqrt(((cp_x - cwp_x)**2) + ((cp_y - cwp_y)**2) + ((cp_z - cwp_z)**2))
+#         threshold = 1.4
+#         if euc_dis < threshold: #WAYPOINT
+#             if self.landed:
+#                 self.landed = False
+#                 return [cwp_x, cwp_y, 1.5]
+            
+#             #Hasn't Landed yet OR risen from landing
+#             curr_img = self.fetchLatestImage                        #Get image 
+#             gray_img = cv2.cvtColor(curr_img, cv2.COLOR_BGR2GRAY)   #convert to grayscale
+#             detector = apriltag.Detector()  
+#             result = detector.detect(gray_img)
+#             t_num = result[0].tag_id
+            
+#             if t_num == 4:
+#                 self.landed = True
+#                 xyz_desired = [cwp_x, cwp_y, 0.1]
+#                 curr_time = time
+            
+#             self.i = self.i+1
+        
+#         return xyz_desired
+
+#     def fetchLatestImage(self):
+#         # Fetch image - renders the camera, saves the rendered image to a file and reads from it. 
+#         path_dir = bpy.data.scenes["Scene"].node_tree.nodes["File Output"].base_path
+
+#         # Render Drone Camera
+#         cam = bpy.data.objects['DownCam']    
+#         bpy.context.scene.camera = cam
+#         bpy.context.scene.render.filepath = os.path.join(path_dir, 'DownCam_latest.png')
+#         bpy.ops.render.render(write_still=True)
+
+#         return cv2.imread(bpy.context.scene.render.filepath)
+
+
 
 
 
