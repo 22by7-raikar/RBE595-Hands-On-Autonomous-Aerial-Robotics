ca68290f86b166237c4e1d8f5222ce2acac4d752
Anuj Pai Raikar <117297932+22by7-raikar@users.noreply.github.com>
2023-11-03 19:45:12 -0400

    Update photos.py

--- Window-Perception/src/photos.py
+++ Window-Perception/src/photos.py
@@ -1,93 +1,3 @@
-    #import bpy
-    #import math
-
-    #def position_camera(cam, angle_x, angle_y, radius, focus_target):
-    #    # Position the camera
-    #    cam.location = (0, -radius, 0)
-    #    
-    #    # Clear existing track_to constraints from the camera
-    #    for constraint in cam.constraints:
-    #        if constraint.type == 'TRACK_TO':
-    #            cam.constraints.remove(constraint)
-
-    #    # Add track_to constraint to point the camera at the focus_target
-    #    track_to = cam.constraints.new(type='TRACK_TO')
-    #    track_to.target = focus_target
-    #    track_to.track_axis = 'TRACK_NEGATIVE_Z'
-    #    track_to.up_axis = 'UP_Y'
-    #    
-    #    # Rotate the camera around the Z-axis
-    #    cam.rotation_euler[2] = math.radians(angle_x)
-    #    
-    #    # Rotate the camera up or down around its local X-axis
-    #    cam.rotation_euler[0] = math.radians(angle_y)
-
-    ## Ensure you're in object mode
-    #bpy.ops.object.mode_set(mode='OBJECT')
-
-    ## Get the focus target and camera
-    #focus_target = bpy.data.objects['Plane.001']
-    #cam = bpy.data.objects['NN_cam']
-
-    ## Parameters for camera positioning
-    #angles_x = [0, 90, 180, 270]  # Around the object in the XZ plane
-    #angles_y = [0, 30, 60, -30, -60]  # Above and below the object
-    #radius = 10  # Distance from the focus_target
-
-    ## Position camera at different angles and render images
-    #render_path = "/home/anuj/Desktop/AerialRobotics/apairaikar_p3a/Dataset/Train/Images/"  # Modify this path as needed
-    #for ax in angles_x:
-    #    for ay in angles_y:
-    #        position_camera(cam, ax, ay, radius, focus_target)
-    #        bpy.context.scene.camera = cam
-    #        bpy.context.scene.render.filepath = f"{render_path}render_{ax}_{ay}.png"
-    #        bpy.ops.render.render(write_still=True)
-
-
-    #import bpy
-    #import math
-
-    #def position_camera(cam, angle_x, angle_y, x,y, focus_target):
-    #    # Position the camera
-    #    cam.location = (x, y, 0)
-    #    
-    #    # Clear existing track_to constraints from the camera
-    #    for constraint in cam.constraints:
-    #        if constraint.type == 'TRACK_TO':
-    #            cam.constraints.remove(constraint)
-
-    #    # Add track_to constraint to point the camera at the focus_target
-    #    track_to = cam.constraints.new(type='TRACK_TO')
-    #    track_to.target = focus_target
-    #    track_to.track_axis = 'TRACK_NEGATIVE_Z'
-    #    track_to.up_axis = 'UP_Y'
-    #    
-    #    # Rotate the camera around the Z-axis
-    #    cam.rotation_euler[2] = math.radians(angle_x)
-    #    
-    #    # Rotate the camera up or down around its local X-axis
-    #    cam.rotation_euler[0] = math.radians(angle_y)
-
-    ## Ensure you're in object mode
-    #bpy.ops.object.mode_set(mode='OBJECT')
-
-    ## Get the focus target and camera
-    #focus_target = bpy.data.objects['Plane.001']
-    #cam = bpy.data.objects['NN_cam.001'] #Change to NN_cam for other camera photo generation
-
-    ## Parameters for camera positioning
-    #angles_x = [0, 90, 180, 270]  # Around the object in the XZ plane
-    #angles_y = [0, 30, 60, -30, -60]  # Above and below the object
-    #radiusy = [0,0.5,1,1.5]  # Distance from the focus_target
-    #radiusx = [0,0.5,1,1.5]
-    ## Position camera at different angles and render images
-    #render_path = "/home/anuj/Desktop/AerialRobotics/apairaikar_p3a/Dataset/Train/Images/"  # Modify this path as needed
-    #for y in radiusy:
-    #    for x in radiusx:
-    #        position_camera(cam, 0, 0, x,y, focus_target)
-    #        bpy.context.scene.camera = cam
-    #        bpy.context.scene.render.filepath = f"{render_path}render{x}_{y}.png"
-    #        bpy.ops.render.render(write_still=True)
 import bpy
 import bmesh
 import math
@@ -143,15 +53,6 @@ def world_to_camera_view(scene, camera, coord):
 
     return x, y
 
-def adjust_lighting(light_name, intensity):
-    light = bpy.data.objects.get(light_name)
-    if light and light.type == 'LIGHT':
-        light.data.energy = intensity
-        print(f"Adjusted {light_name} intensity to {intensity}")
-    else:
-        print(f"Light source {light_name} not found or is not a lamp object.")
-
-
 def main():
     bpy.ops.object.mode_set(mode='OBJECT')
     focus_target = bpy.data.objects.get('Plane')
@@ -167,9 +68,7 @@ def main():
     # For Eevee or Cycles, you might control lighting through environment or lamp strengths
 
     light_name = "MyLightSource"
-    intensities = [0.0,10,20,30,100]
-
-    render_path = "/home/anuj/Desktop/AerialRobotics/apairaikar_p3a/datasets/mydataset/raw_images/train/1"
+    render_path = "/home/anuj/Desktop/AerialRobotics/apairaikar_p3a/datasets/mydataset/raw_images/train/1/"
     json_file_path = "/home/anuj/Desktop/AerialRobotics/apairaikar_p3a/datasets/mydataset/annotations/image_vertex_mapping_1.json"
     image_width = bpy.context.scene.render.resolution_x * bpy.context.scene.render.resolution_percentage / 100
     image_height = bpy.context.scene.render.resolution_y * bpy.context.scene.render.resolution_percentage / 100
@@ -178,14 +77,10 @@ def main():
     for az in radiusz:
         for ay in radiusy:
             for ax in radiusx:
-                for intensity in intensities:
                     position_camera(cam, 0, 0, ax, ay, az, focus_target)
                     bpy.context.scene.camera = cam
 
-                    # Adjust the lighting condition
-                    adjust_lighting(light_name, intensity)
-
-                    bpy.context.scene.render.filepath = f"multcond1{render_path}render{ax}_{ay}_{az}_{intensity}_1.png"
+                    bpy.context.scene.render.filepath = f"{render_path}render{ax}_{ay}_{az}_1.png"
                     bpy.ops.render.render(write_still=True)
 
                     image_file_path = f"{render_path}render{ax}_{ay}_{az}_1.png"
